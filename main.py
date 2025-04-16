b0beffced7e2c2dc5a6c9ef6e8e8b63b241d5b2b
ASHBRO-12 <asherkoko@gmail.com>
2025-04-16 18:59:32 +0100

    3rd commit

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-name = "Penguin"
-age = 15
-is_student = True
-weight = 38.5
+#input a word
+text = str(input("Enter a string: "))
 
-print("Name :", name)
-print("Data type of name is :", type(name))
-print("Age :", age)
-print("Data type of age is :", type(age))
-print("Is Student :", is_student)
-print("Data type of if he is a student is :", type(is_student))
-print("Weight :", weight)
-print("Data type of weight is :", type(weight))
+# Reverse String 
+# using step value as -1 to iterate in reverse
+revText = text[::-1] 
+text = revText
 
-print("\n After Type Casting....")
-age = str(age)
-print(age)
-print("Data type of age is :", type(age))
-weight = int(weight)
-print(weight)
-print("Data type of weight is :", type(weight))
+print("Reverse of Given String is:")
+print(text)
 

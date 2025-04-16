fbb8bebb4760be67c4fd6957f3c91cd2423aa4ca
ASHBRO-12 <asherkoko@gmail.com>
2025-04-16 18:54:45 +0100

    2nd commit

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-a = 5
-print("type of a: ", type(a))
-b=2.5
-print("type of b: ", type(b))
-c= "coding"
-print("type of c: ", type(c))
-d= True
-print("type of d: ", type(d))
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data type of name is :", type(name))
+print("Age :", age)
+print("Data type of age is :", type(age))
+print("Is Student :", is_student)
+print("Data type of if he is a student is :", type(is_student))
+print("Weight :", weight)
+print("Data type of weight is :", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is :", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is :", type(weight))
+

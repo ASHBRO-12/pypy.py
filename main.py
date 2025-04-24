546aa6bfeab5007259ee26f392e0dce15f43d322
ASHBRO-12 <asherkoko@gmail.com>
2025-04-24 19:08:51 +0100

    6th commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-i = int(input("Enter a number : "))
+number = int(input("Enter a number to check: "))
+print("Number to be checked :", number)
 
-if (i < 15):
-    print("i is less than fifteen.")
-    print("I'm in the if statement.")
+if number%2==0:
+    print("This is an even number.")
 else:
-    print("i is greater than fifteen.")
-    print("I'm in the else statement.")
-print("I'm not in the if or else statement.")
+    print("This is an odd number.")

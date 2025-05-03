506c904b5bbe7a771bd266e1a10c689cd2f69b38
ASHBRO-12 <asherkoko@gmail.com>
2025-05-03 18:51:05 +0100

    8th commit

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-a = 10
-b = 12
-c = 0
+n = int(input("Enter the number whose sum you want to find: "))
+sum = 0
 
-if a and b and c:
-    print("All the numbers have boolean value as True.")
-else:
-    print("At least one number has boolean value as False.")
-
-a = 10
-b = -10
-c = 0
-
-if a > 0 or b > 0:
-    print("Either of the numbers is greater than 0.")
-else:
-    print("No number is greater than 0.")
-
-if b > 0 or c > 0:
-    print("Either of the numbers is greater than 0.")
-else:
-    print("No number is greater than 0.")
+for i in range(1, n+1):
+    sum = sum+1
+    print("\nSum =", sum)

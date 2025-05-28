48da72802269f12f9511b4f5b74343512e839ed6
ASHBRO-12 <asherkoko@gmail.com>
2025-05-28 18:33:19 +0100

    14th commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-a = input("Enter a word: ")
-for i in a:
-    if (i == "A"):
-        print("'A' is found!")
-        break
+for x in range(20):
+    if x % 20 == 0:
+        print("twist")
+    elif x % 15 == 0:
+        pass
+    elif x % 5 == 0:
+        print("fizz")
+    elif x % 3 == 0:
+        print("buzz")
     else:
-        print("'A' isn't found...")
+        print(x)
+    
